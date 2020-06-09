4e6880339c214d067a115e6bf71dce0fb7ac3407
Jay Viloria <jviloria96744@gmail.com>
2020-06-08 17:33:54 -0700

    Removed filter context and moved filter functions to photo context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import FilterState from "./context/filter/FilterState";
 import PhotoState from "./context/photo/PhotoState";
 
 ReactDOM.render(
   <PhotoState>
-    <FilterState>
-      <App />
-    </FilterState>
+    <App />
   </PhotoState>,
   document.getElementById("root")
 );
